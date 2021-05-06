043a79a8f4f0c825e1de4dc5b14b1a1ec33ba45a
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-06 23:34:41 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
+  if (x <= 400){
+    return `This one is on me!`
+  } else if (x > 2000 && x < 2499){
+    return `I will gladly take your thirty bucks.`
+  }else if (x > 2500){
+    return `No can do.`
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(x){
+  if (x === 'NYC'){
+    return `Ok, sounds good.`
+  } else {
+    return `No go.`
+  }
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(x){
+  if (x === 'generous'){
+    return `Thank you so much.`
+  } else if (x === 'not as generous'){
+    return `Thank you.`
+  } else {
+    return `Bye.`;
+  }
   // Write your code here!
 }
